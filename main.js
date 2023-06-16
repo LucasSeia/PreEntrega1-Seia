c203a6e73ca29cec584e8fec67d5a4883f573ec8
LucasDev <lucasseia04@gmail.com>
2023-06-16 17:52:02 -0300

    Cambios en el codigo

--- main.js
+++ main.js
@@ -1,67 +1,46 @@
-/* let tarea = prompt('Ingresa una tarea');
-
-if(tarea != ''){
-    console.log(tarea);
-}else{
-    alert("Debes ingresar un valor");
-}
-
- */
-
-/* let listaTareas = 'Lista de Tareas:'; 
-
-let titulo = prompt("Ingrese el título de la tarea:");
-let descripcion = prompt("Ingrese la descripción de la tarea:");
-
-
-
-function agregarTarea() {
-  while(true){
-        if (confirm('¿Quieres agregar otra?')){
-            titulo = prompt("Ingrese el título de la tarea:");
-            descripcion = prompt("Ingrese la descripción de la tarea:");
-
-            // Crear un objeto para representar la tarea
-            let tarea = {
-                titulo: titulo,
-                descripcion: descripcion,
-            };
-            
-            console.log('La terea es: ' + titulo)
-            console.log('La descripción de la tarea es: ' + descripcion)
-        } else {
-            alert('¡No te olvides de realizarlas!')
-            break;
-        }
-    }
-}
-
-console.log('La terea es: ' + titulo)
-console.log('La descripción de la tarea es: ' + descripcion)
-
-agregarTarea() */
-
-let titulo = prompt("Ingrese el título de la tarea:");
-let descripcion = prompt("Ingrese la descripción de la tarea:");
+let tareas = {};
 
 function agregarTarea() {
   while (true) {
-    console.log('La tarea es: ' + titulo);
-    console.log('La descripción de la tarea es: ' + descripcion);
+    titulo = prompt("Ingrese el título de la tarea:");
+    descripcion = prompt("Ingrese la descripción de la tarea:");
+
+    tareas[titulo] = descripcion;
 
     if (!confirm('¿Quieres agregar otra?')) {
       alert('¡No te olvides de realizarlas!');
       break; // Salir del bucle si no se desea agregar más tareas
     }
-
-    titulo = prompt("Ingrese el título de la tarea:");
-    descripcion = prompt("Ingrese la descripción de la tarea:");
   }
 }
 
-agregarTarea(); // Llamar a la función para comenzar a agregar tareas
+function verListaTareas() {
+  console.log('--- Lista de Tareas ---');
+  for (let titulo in tareas) {
+    console.log('Título: ' + titulo);
+    console.log('Descripción: ' + tareas[titulo]);
+    console.log('-----------------------');
+  }
+}
 
-// console.log(listaTareas);
-console.log('La tarea es: ' + titulo);
-console.log('La descripción de la tarea es: ' + descripcion);
+function eliminarTarea() {
+  let titulo = prompt("Ingrese el título de la tarea que desea eliminar:");
+  if (tareas.hasOwnProperty(titulo)) {
+    let confirmarEliminar = confirm('¿Estás seguro de que deseas eliminar la tarea "' + titulo + '"?');
+    if (confirmarEliminar) {
+      delete tareas[titulo];
+      console.log('La tarea "' + titulo + '" ha sido eliminada correctamente.');
+    } else {
+      console.log('Se canceló la eliminación de la tarea "' + titulo + '".');
+    }
+  } else {
+    console.log('No se encontró ninguna tarea con el título "' + titulo + '".');
+  }
+}
+
+
+agregarTarea(); // Llamar a la función para comenzar a agregar tareas
+verListaTareas();
+eliminarTarea();
+
 
