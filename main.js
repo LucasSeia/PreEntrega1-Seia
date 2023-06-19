80ada22115acf18a7fa087c5625f5d06d96e4f26
LucasDev <lucasseia04@gmail.com>
2023-06-19 17:10:55 -0300

    Calculadora

--- main.js
+++ main.js
@@ -1,46 +1,30 @@
-let tareas = {};
+function calcularOperacion(num1, num2, operacion) {
+  let resultado;
 
-function agregarTarea() {
-  while (true) {
-    titulo = prompt("Ingrese el título de la tarea:");
-    descripcion = prompt("Ingrese la descripción de la tarea:");
-
-    tareas[titulo] = descripcion;
-
-    if (!confirm('¿Quieres agregar otra?')) {
-      alert('¡No te olvides de realizarlas!');
-      break; // Salir del bucle si no se desea agregar más tareas
-    }
-  }
-}
-
-function verListaTareas() {
-  console.log('--- Lista de Tareas ---');
-  for (let titulo in tareas) {
-    console.log('Título: ' + titulo);
-    console.log('Descripción: ' + tareas[titulo]);
-    console.log('-----------------------');
-  }
-}
-
-function eliminarTarea() {
-  let titulo = prompt("Ingrese el título de la tarea que desea eliminar:");
-  if (tareas.hasOwnProperty(titulo)) {
-    let confirmarEliminar = confirm('¿Estás seguro de que deseas eliminar la tarea "' + titulo + '"?');
-    if (confirmarEliminar) {
-      delete tareas[titulo];
-      console.log('La tarea "' + titulo + '" ha sido eliminada correctamente.');
-    } else {
-      console.log('Se canceló la eliminación de la tarea "' + titulo + '".');
-    }
+  if (operacion === '+') {
+    resultado = num1 + num2;
+  } else if (operacion === '-') {
+    resultado = num1 - num2;
+  } else if (operacion === '*') {
+    resultado = num1 * num2;
+  } else if (operacion === '/') {
+    resultado = num1 / num2;
   } else {
-    console.log('No se encontró ninguna tarea con el título "' + titulo + '".');
+    return "Operación no válida.";
   }
+
+  return "El resultado de la operación es: " + resultado;
 }
 
+let continuar;
 
-agregarTarea(); // Llamar a la función para comenzar a agregar tareas
-verListaTareas();
-eliminarTarea();
+do {
+  let numero1 = parseFloat(prompt("Ingrese el primer número:"));
+  let numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+  let operacion = prompt("Ingrese la operación a realizar (+, -, *, /):");
 
+  let resultadoOperacion = calcularOperacion(numero1, numero2, operacion);
+  console.log(resultadoOperacion);
 
+  continuar = prompt("¿Desea realizar otra operación? (Sí/No)").toLowerCase();
+} while (continuar === 'sí' || continuar === 'si');
