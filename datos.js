bf5b1a96793c54b6df5acff3cbc828bbc6df7d45
LucasDev <lucasseia04@gmail.com>
2023-06-30 18:02:07 -0300

    Empezando segunda pre entrega

---
+++ datos.js
@@ -0,0 +1,210 @@
+const seleccionArgentina = [
+	{
+		nombre: 'Emiliano',
+        apellido: 'Martínez',
+		club: 'Aston Villa',
+		edad: 30,
+		posicion: 'Arquero',
+		partidosJudados: 7
+	},
+	{ 
+        nombre: 'Gerónimo', 
+        apellido: 'Rulli', 
+        club: 'Ajax', 
+        edad: 31, 
+        posicion: 'Arquero', 
+        partidosJudados: 0
+    },
+	{
+		nombre: 'Franco',
+        apellido: 'Armani',
+		club: 'River Plate',
+		edad: 36,
+		posicion: 'Arquero',
+		partidosJudados: 0
+	},
+	{
+		nombre: 'Juan',
+        apellido: 'Foyth',
+		club: 'Villareal',
+		edad: 25,
+		posicion: 'Defensor',
+		partidosJudados: 1
+	},
+    {
+		nombre: 'Gonzalo',
+        apellido: 'Montiel',
+		club: 'Sevilla',
+		edad: 26,
+		posicion: 'Defensor',
+		partidosJudados: 4
+	},
+    {
+		nombre: 'Nahuel',
+        apellido: 'Molina',
+		club: 'Atletico Madrid',
+		edad: 25,
+		posicion: 'Defensor',
+		partidosJudados: 7
+	},
+    {
+		nombre: 'Cristian',
+        apellido: 'Romero',
+		club: 'Tottenham Hotspur',
+		edad: 25,
+		posicion: 'Defensor',
+		partidosJudados: 6
+	},
+    {
+		nombre: 'German',
+        apellido: 'Pezzella',
+		club: 'Real Betis',
+		edad: 32,
+		posicion: 'Defensor',
+		partidosJudados: 3
+	},
+    {
+		nombre: 'Nicolas',
+        apellido: 'Otamendi',
+		club: 'Benfica',
+		edad: 35,
+		posicion: 'Defensor',
+		partidosJudados: 7
+	},
+    {
+		nombre: 'Lisandrio',
+        apellido: 'Martinez',
+		club: 'Manchester United',
+		edad: 25,
+		posicion: 'Defensor',
+		partidosJudados: 4
+	},
+    {
+		nombre: 'Marcos',
+        apellido: 'Acuña',
+		club: 'Sevilla',
+		edad: 31,
+		posicion: 'Defensor',
+		partidosJudados: 6
+	},
+    {
+		nombre: 'Nicolas',
+        apellido: 'Tagliafico',
+		club: 'Olympique Lyon',
+		edad: 30,
+		posicion: 'Defensor',
+		partidosJudados: 5
+	},
+    {
+		nombre: 'Leandro',
+        apellido: 'Paredes',
+		club: 'PSG',
+		edad: 29,
+		posicion: 'Mediocentro',
+		partidosJudados: 5
+	},
+    {
+		nombre: 'Rodrigo',
+        apellido: 'de Paul',
+		club: 'Atletico Madrid',
+		edad: 29,
+		posicion: 'Mediocentro',
+		partidosJudados: 7
+	},
+    {
+		nombre: 'Exequiel',
+        apellido: 'Palacios',
+		club: 'Bayer Leverkusen',
+		edad: 24,
+		posicion: 'Mediocentro',
+		partidosJudados: 3
+	},
+    {
+		nombre: 'Alejandro',
+        apellido: 'Gomez',
+		club: 'Sevilla',
+		edad: 35,
+		posicion: 'Mediocentro',
+		partidosJudados: 2
+	},
+    {
+		nombre: 'Guido',
+        apellido: 'Rodriguez',
+		club: 'Real Betis',
+		edad: 29,
+		posicion: 'Mediocentro',
+		partidosJudados: 1
+	},
+    {
+		nombre: 'Alexis',
+        apellido: 'Mac Allister',
+		club: 'Liverpool',
+		edad: 24,
+		posicion: 'Mediocentro',
+		partidosJudados: 6
+	},
+    {
+		nombre: 'Enzo',
+        apellido: 'Fernandez',
+		club: 'Chelsea',
+		edad: 22,
+		posicion: 'Mediocentro',
+		partidosJudados: 7
+	},
+    {
+		nombre: 'Julian',
+        apellido: 'Alverez',
+		club: 'Manchester City',
+		edad: 23,
+		posicion: 'Delantero',
+		partidosJudados: 6
+	},
+    {
+		nombre: 'Lionel',
+        apellido: 'Messi',
+		club: 'Inter Miami',
+		edad: 36,
+		posicion: 'Delantero',
+		partidosJudados: 7
+	},
+    {
+		nombre: 'Angel',
+        apellido: 'Di Maria',
+		club: 'Benfica',
+		edad: 35,
+		posicion: 'Delantero',
+		partidosJudados: 3
+	},
+    {
+		nombre: 'Angel',
+        apellido: 'Correa',
+		club: 'Altletico Madrid',
+		edad: 28,
+		posicion: 'Delantero',
+		partidosJudados: 1
+	},
+    {
+		nombre: 'Thiago',
+        apellido: 'Almada',
+		club: 'Atlanta United',
+		edad: 22,
+		posicion: 'Delantero',
+		partidosJudados: 1
+	},
+    {
+		nombre: 'Paulo',
+        apellido: 'Dybala',
+		club: 'Roma',
+		edad: 29,
+		posicion: 'Delantero',
+		partidosJudados: 2
+	},
+    {
+		nombre: 'Lautaro',
+        apellido: 'Martinez',
+		club: 'Inter',
+		edad: 25,
+		posicion: 'Delantero',
+		partidosJudados: 6
+	}
+]
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function calcularOperacion(num1, num2, operacion) {
+/* function calcularOperacion(num1, num2, operacion) {
   let resultado;
 
   if (operacion === '+') {
@@ -29,4 +29,92 @@ do {
   console.log(resultadoOperacion);
 
   continuar = prompt("¿Desea realizar otra operación? (Sí/No)").toLowerCase();
-} while (continuar === 'sí' || continuar === 'si');
+} while (continuar === 'sí' || continuar === 'si'); */
+
+let nombre = prompt("Solicitar nombre")
+let apellido = prompt("Solicitar apellido")
+let club = prompt("Solicitar club")
+let edad = prompt("Solicitar edad")
+let posicion = prompt("Solicitar posicion")
+let partidosJudados = prompt("Solicitar partidos jugados")
+
+const datosBusqueda = {
+    nombre: nombre,
+	apellido: apellido,
+	club: club,
+	edad: edad,
+    posicion: posicion,
+	partidosJudados: partidosJudados
+}
+
+function mostrarSeleccion(seleccionArgentina){
+    seleccionArgentina.forEach( jugador => {
+        console.log(jugador.nombre + " " + jugador.apellido + " Club: " + jugador.club + " " + jugador.edad + " Años " + jugador.posicion + " Posición: " + jugador.partidosJudados + " Partidos Jugados ")
+
+        document.write("<p style='padding:15px; font-size:1.5rem; text-align:center; color:#efb810'>" + jugador.nombre + " " + jugador.apellido + " Club: " + jugador.club + " " + jugador.edad + " Años " + jugador.posicion + " Posición: " + jugador.partidosJudados + " Partidos Jugados " + "</p>")
+
+    })
+}
+
+function noResultados(){
+    console.log("No hay Resultados")
+    document.write("<p style='color:red; padding:10px; font-size:2rem; text-align:center'>No hay Resultados</p>")
+}
+
+
+function filtrarNombre(jugador){
+    if(datosBusqueda.nombre){
+        return jugador.nombre == datosBusqueda.nombre
+    }
+    return jugador;
+}
+
+function filtrarApellido(jugador){
+    if(datosBusqueda.npellido){
+        return jugador.apellido == datosBusqueda.apellido
+    }
+    return jugador;
+}
+
+function filtrarClub(jugador){
+    if(datosBusqueda.club){
+        return jugador.club >= datosBusqueda.club
+    }
+    return jugador;
+}
+
+function filtrarEdad(jugador){
+    if(datosBusqueda.edad){
+        return jugador.edad <= datosBusqueda.edad
+    }
+    return jugador;
+}
+
+function filtrarPosicion(jugador){
+    if(datosBusqueda.posicion){
+        return jugador.posicion == datosBusqueda.posicion
+    }
+    return jugador;
+}
+
+function filtrarPartidosJugados(jugador){
+    if(datosBusqueda.partidosJudados){
+        return jugador.partidosJudados == datosBusqueda.partidosJudados
+    }
+    return jugador;
+}
+
+
+
+function filtrarSeleccion(){
+    let resultado = seleccionArgentina.filter(filtrarNombre).filter(filtrarApellido).filter(filtrarClub).filter(filtrarEdad).filter(filtrarPosicion).filter(filtrarPartidosJugados);
+    console.log(resultado.length)
+    if(resultado.length){
+        mostrarSeleccion(resultado)
+    }else {
+        noResultados()
+    }
+}
+
+
+filtrarSeleccion();
